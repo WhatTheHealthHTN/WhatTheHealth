31fede9f09dc8c60a297804b2e30f455de6a3739
Leon Si <leon@leonzalion.com>
2022-09-17 19:04:01 -0400

    general health tips

--- src/components/sections/health-tips.tsx
+++ src/components/sections/health-tips.tsx
@@ -18,22 +18,25 @@ const healthTips = [
 
 export default function HealthTipsSection() {
 	return (
-		<div className='flex flex-row self-stretch h-full p-4 gap-4'>
-			{healthTips.map((healthTip, i) => {
-				return (
-					<Box key={i} rounded='2xl' p={5} shadow='md' className='flex flex-col gap-4'>
-						<div className='font-medium'>
-							{healthTip.title}
-						</div>
-						<div className='text-gray-700 text-sm'>
-							{healthTip.description}
-						</div>
-						<div className='flex flex-row text-sm items-center text-[#E338F9] gap-1'>
-							Read More <BsChevronRight size={8} />
-						</div>
-					</Box>
-				)
-			})}
+		<div>
+			<div className='font-bold text-xl mt-4 mb-2'>General Health Tips</div>
+			<div className='flex flex-col gap-4'>
+				{healthTips.map((healthTip, i) => {
+					return (
+						<Box key={i} rounded='2xl' p={5} shadow='md' className='flex flex-col gap-4'>
+							<div className='font-medium'>
+								{healthTip.title}
+							</div>
+							<div className='text-gray-700 text-sm'>
+								{healthTip.description}
+							</div>
+							<div className='flex flex-row text-sm items-center text-[#E338F9] gap-1'>
+								Read More <BsChevronRight size={8} />
+							</div>
+						</Box>
+					)
+				})}
+			</div>
 		</div>
 	)
 }
