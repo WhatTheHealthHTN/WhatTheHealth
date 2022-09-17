cfa1369bdacd70fd292aa4106ffd338f6bec8ae6
Leon Si <leon@leonzalion.com>
2022-09-17 14:12:08 -0400

    better ui

--- src/components/recommendation-card.tsx
+++ src/components/recommendation-card.tsx
@@ -26,10 +26,19 @@ export function RecommendationTodo({ todo }: { todo: Todo }) {
 		timestamp: 0
 	})
 	const [isRecalculating, setIsRecalculating] = useState(false)
+	const [isDeleting, setIsDeleting] = useState(false)
 	const [timeElapsed, setTimeElapsed] = useState(0)
 	const deleteTodo = useDashboardStore.use.deleteTodo()
 
 	const threeSeconds = 3 * 1000;
+
+	useEffect(() => {
+		if (isDeleting) {
+			setTimeout(() => {
+				deleteTodo(todo.id)
+			}, 500)
+		}
+	}, [isDeleting, deleteTodo, todo.id])
 
 	useAnimationFrame(() => {
 		if (checkedData.checked) {
@@ -41,7 +50,7 @@ export function RecommendationTodo({ todo }: { todo: Todo }) {
 				setIsRecalculating(true)
 
 				setTimeout(() => {
-					deleteTodo(todo.id)
+					setIsDeleting(true)
 				}, 2_000)
 			}
 		}
@@ -51,38 +60,40 @@ export function RecommendationTodo({ todo }: { todo: Todo }) {
 	const completed = (timeElapsed - 500) / threeSeconds * 100;
 
 	return (
-		<Box rounded='3xl' p={5} borderWidth='1.5px' className={cx('flex flex-row gap-4 items-center transition-colors relative', checkedData.checked ? 'bg-[rgb(250,251,255)]' : '')}>
-			{isRecalculating && (
-				<div className='flex flex-row items-center gap-4 text-gray-500 p-4 absolute top-1/2 -translate-y-1/2 left-[10px]'>
-					<Spinner />
-					<div>Recalculating health score...</div>
-				</div>
-			)}
-
-			<Checkbox className={isRecalculating ? 'invisible' : 'visible'} checked={checkedData.checked} onChange={(e) => {
-				setCheckedData({
-					checked: (e.target as any).checked,
-					timestamp: Date.now()
-				})
-			}} />
-			<div className={cx('relative', isRecalculating ? 'invisible' : 'visible')}>
-				<motion.div className={cx('flex flex-col absolute -translate-x-1/2 -translate-y-1/2 top-1/2 ml-[70px]', checkedData.checked ? 'pointer-events-none' : 'pointer-events-auto')} animate={checkedData.checked ? { opacity: 0, left: -5 } : {}} transition={{ delay: checkedData.checked ? 0 : 0.5, duration: checkedData.checked ? 0.5 : 0.2 }}>
-					<span className='font-medium'>
-						{todo.title}
-					</span>
-				</motion.div>
-
-				<motion.div className={cx(checkedData.checked ? 'pointer-events-auto' : 'pointer-events-none')} initial={{ opacity: 0, marginLeft: -5 }} animate={checkedData.checked ? { opacity: 1, marginLeft: 0 } : {}} transition={{ delay: checkedData.checked ? 0.5 : 0, duration: checkedData.checked ? 0.2 : 0.5 }} >
-					<div className='flex flex-col'>
-						<span className='font-medium flex flex-row items-center gap-2'>
-							Task completed! <span className='text-xl'>🎉</span>
-						</span>
-						<div className='mb-2 text-gray-500'>You have <span className='font-bold'>{Math.ceil((3 - ((timeElapsed - 500) / 1000)))}s</span> to undo the action before the task disappears from the list.</div>
-						<Progress value={completed} rounded='md' height='7px' />
+		<motion.div initial={{ opacity: 1 }} animate={isDeleting ? { opacity: 0, marginLeft: -10 } : {}} transition={{ duration: 0.5 }}>
+			<Box rounded='3xl' p={5} borderWidth='1.5px' className={cx('flex flex-row gap-4 items-center transition-colors relative', checkedData.checked ? 'bg-[rgb(250,251,255)]' : '')}>
+				{isRecalculating && (
+					<div className='flex flex-row items-center gap-4 text-gray-500 p-4 absolute top-1/2 -translate-y-1/2 left-[10px]'>
+						<Spinner />
+						<div>Recalculating health score...</div>
 					</div>
-				</motion.div>
-			</div>
-		</Box>
+				)}
+
+				<Checkbox className={isRecalculating ? 'invisible' : 'visible'} checked={checkedData.checked} onChange={(e) => {
+					setCheckedData({
+						checked: (e.target as any).checked,
+						timestamp: Date.now()
+					})
+				}} />
+				<div className={cx('relative', isRecalculating ? 'invisible' : 'visible')}>
+					<motion.div className={cx('flex flex-col absolute -translate-x-1/2 -translate-y-1/2 top-1/2 ml-[70px]', checkedData.checked ? 'pointer-events-none' : 'pointer-events-auto')} animate={checkedData.checked ? { opacity: 0, left: -5 } : {}} transition={{ delay: checkedData.checked ? 0 : 0.5, duration: checkedData.checked ? 0.5 : 0.2 }}>
+						<span className='font-medium'>
+							{todo.title}
+						</span>
+					</motion.div>
+
+					<motion.div className={cx(checkedData.checked ? 'pointer-events-auto' : 'pointer-events-none')} initial={{ opacity: 0, marginLeft: -5 }} animate={checkedData.checked ? { opacity: 1, marginLeft: 0 } : {}} transition={{ delay: checkedData.checked ? 0.5 : 0, duration: checkedData.checked ? 0.2 : 0.5 }} >
+						<div className='flex flex-col'>
+							<span className='font-medium flex flex-row items-center gap-2'>
+								Task completed! <span className='text-xl'>🎉</span>
+							</span>
+							<div className='mb-2 text-gray-500'>You have <span className='font-bold'>{Math.ceil((3 - ((timeElapsed - 500) / 1000)))}s</span> to undo the action before the task disappears from the list.</div>
+							<Progress value={completed} rounded='md' height='7px' />
+						</div>
+					</motion.div>
+				</div>
+			</Box>
+		</motion.div>
 	)
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,8 +27,8 @@ const Home: NextPage = () => {
 				</span>
 				, fully.
 			</motion.div>
-			<motion.div initial={{ y: 0, opacity: 0 }} animate={{ opacity: 1, y: 10 }} transition={{ delay: 0.2, duration: 0.5 }} className='max-w-2xl mt-1 text-center text-gray-600 font-medium mx-auto text-lg px-4 flex flex-col items-center gap-4 mb-6'>
-				HealthApp gathers all your data in one place, using AI to provide actionable recommendations to improve your overall health.
+			<motion.div initial={{ y: 0, opacity: 0 }} animate={{ opacity: 1, y: 10 }} transition={{ delay: 0.2, duration: 0.5 }} className='max-w-2xl mt-1 text-center text-gray-600 font-medium mx-auto text-lg px-4 gap-4 mb-6'>
+				HealthApp gathers all your data in one place, using <span className='font-bold bg-gradient-to-r from-[#1f51ffaa] to-[#b941ff] text-transparent bg-clip-text text-lg'>Artificial&nbsp;Intelligence</span> to provide actionable recommendations to improve your overall health.
 			</motion.div>
 
 			<motion.div initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{ delay: 0.7, duration: 0.5, ease: 'circOut' }} className='flex flex-col items-center'>
