8d851a8df08693ce1b8a599db5ac3ff43d8f2ee4
Leon Si <leon@leonzalion.com>
2022-09-17 18:11:54 -0400

    colorized health diagnostics

--- src/components/sections/health-diagnosis.tsx
+++ src/components/sections/health-diagnosis.tsx
@@ -34,13 +34,14 @@ const HealthDiagnosisSection = () => {
 						</Menu>
 					</div>
 				</div>
+
 				<HealthPieChart data={overallData} />
 				<div className='px-2 self-stretch flex flex-col gap-2 mr-4'>
-					<HealthLineBar text='Blood' value={88} color='#ff0000' />
-					<HealthLineBar text='Pollution' value={72} color='#ff0000' />
-					<HealthLineBar text='Exercise' value={60} color='#ff0000' />
-					<HealthLineBar text='Nutrition' value={75} color='#ff0000' />
-					<HealthLineBar text='Sleep' value={80} color='#ff0000' />
+					<HealthLineBar text='Blood' value={88} fromColor='#ff0000aa' toColor='#ff0000' />
+					<HealthLineBar text='Pollution' value={72} fromColor='#252525aa' toColor='#252525' />
+					<HealthLineBar text='Exercise' value={60} fromColor='#00008Baa' toColor='#00008B' />
+					<HealthLineBar text='Nutrition' value={75} fromColor='#006400aa' toColor='#006400' />
+					<HealthLineBar text='Sleep' value={80} fromColor='#301934aa' toColor='#301934' />
 				</div>
 			</div>
 		</Box>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Script from "next/script";
 const HomeHeader = () => {
 	return (
 		<motion.div initial={{ marginTop: -10, opacity: 0 }} animate={{ opacity: 1, marginTop: 0 }} className='flex flex-row py-4 px-8 gap-2 items-center'>
-			<div className='text-white font-bold text-2xl'>HealthApp</div>
+			<div className='from-[#E338F9] to-[#FEC93A] bg-clip-text bg-gradient-to-r text-transparent font-bold text-2xl'>HealthApp</div>
 			<div className='flex-1'></div>
 			<Button bgColor='#E338F9' color='white' _hover={{ backgroundColor: '#E338F9AA' }}>Login</Button>
 			<Button bgColor='#E1A401' _hover={{ backgroundColor: '#E1A401AA' }} color='white'>Sign up</Button>
@@ -17,31 +17,30 @@ const HomeHeader = () => {
 
 const Home: NextPage = () => {
 	return (
-		<div className='relative'>
-			<div className='absolute inset-0 bg-black -z-[5] pointer-events-none'></div>
-			<div className="min-h-screen overflow-x-hidden">
-				<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 1 }} id='three-container' className='absolute top-[400px] xs:top-[350px] pointer-events-none -z-[1] overflow-hidden max-w-full'></motion.div>
-				<Script async src='/threejs.js' />
-				<HomeHeader />
-				<motion.div className="text-center font-bold text-6xl text-white pt-20 px-8 max-w-3xl mx-auto z-1"
-					transition={{ ease: "easeOut", duration: 0.5 }}
-					initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }}>
-					Understand your{" "}
-					<span className="from-[#E338F9] to-[#FEC93A] bg-clip-text bg-gradient-to-r text-transparent z-1">
-						health
-					</span>
-					, fully.
-				</motion.div>
-				<motion.div initial={{ y: 0, opacity: 0 }} animate={{ opacity: 1, y: 10 }} transition={{ delay: 0.2, duration: 0.5 }} className='max-w-2xl mt-1 text-center text-gray-400 font-medium mx-auto text-lg px-4 gap-4 mb-6'>
-					HealthApp gathers all your data in one place, using <span className='font-bold bg-gradient-to-r from-[#83eeff] to-[#b941ff] text-transparent bg-clip-text text-lg'>Artificial&nbsp;Intelligence</span> to provide actionable recommendations to improve your overall health.
-				</motion.div>
+		<div className="min-h-screen overflow-x-hidden bg-gradient-to-r">
+			<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 1 }} id='three-container' className='absolute top-[400px] xs:top-[350px] pointer-events-none -z-[1] overflow-hidden max-w-full'></motion.div>
+			<HomeHeader />
+			<motion.div className="text-center font-bold text-6xl text-black pt-20 px-8 max-w-3xl mx-auto z-1"
+				transition={{ ease: "easeOut", duration: 0.5 }}
+				initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }}>
+				Understand your{" "}
+				<span className="from-[#E338F9] to-[#FEC93A] bg-clip-text bg-gradient-to-r text-transparent z-1">
+					health
+				</span>
+				, fully.
+			</motion.div>
+			<motion.div initial={{ y: 0, opacity: 0 }} animate={{ opacity: 1, y: 10 }} transition={{ delay: 0.2, duration: 0.5 }} className='max-w-2xl mt-1 text-center text-gray-500 font-medium mx-auto text-lg px-4 gap-4 mb-6'>
+				HealthApp gathers all your data in one place, using <span className='font-bold bg-gradient-to-r from-[#469aea] to-[#b941ff] text-transparent bg-clip-text text-lg'>Artificial&nbsp;Intelligence</span> to provide actionable recommendations to improve your overall health.
+			</motion.div>
 
-				<motion.div initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{ delay: 0.7, duration: 0.5, ease: 'circOut' }} className='flex flex-col items-center'>
-					<Link href='/dashboard'>
-						<button className='text-white rounded-md px-4 py-2 font-bold bg-gradient-to-r from-[#E338F9] to-[#FEC93A] hover:opacity-70 transition-all text-lg'>Get Started</button>
-					</Link>
-				</motion.div>
-			</div>
+			<motion.div initial={{ opacity: 0, scale: 0.9 }} animate={{ opacity: 1, scale: 1 }} transition={{ delay: 0.7, duration: 0.5, ease: 'circOut' }} className='flex flex-col items-center'>
+				<Link href='/dashboard'>
+					<button className='text-white rounded-md px-4 py-2 font-bold bg-gradient-to-r from-[#E338F9] to-[#FEC93A] hover:opacity-70 transition-all text-lg'>Get Started</button>
+				</Link>
+			</motion.div>
+
+			<Script src='/public/threejs.js' />
+			<canvas id="background" />
 		</div>
 	);
 };
