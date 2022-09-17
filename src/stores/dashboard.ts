795e9b8be25c228b633bc046a1cad5fb06042bb4
Leon Si <leon@leonzalion.com>
2022-09-17 13:56:14 -0400

    spinner

--- src/stores/dashboard.ts
+++ src/stores/dashboard.ts
@@ -5,25 +5,29 @@ import { createSelectors } from '~/utils/store'
 interface DashboardStoreState {
 	searchQuery: string, setSearchQuery: (searchQuery: string) => void
 	todos: Todo[]
-	deleteTodo: () => number;
+	deleteTodo: (id: number) => void;
 }
 
 export const useDashboardStore = createSelectors(create<DashboardStoreState>((set) => ({
 	searchQuery: '',
-	todos: [{
-		id: 1,
-		tag: 'exercise',
-		tagColor: 'orange',
-		title: 'Get more exercise'
-	},
-	{
-		id: 2,
-		tag: 'water',
-		tagColor: 'blue',
-		title: 'Drink more water'
-	}],
+	todos: [
+		{
+			id: 1,
+			tag: 'exercise',
+			tagColor: 'orange',
+			title: 'Get more exercise'
+		},
+		{
+			id: 2,
+			tag: 'water',
+			tagColor: 'blue',
+			title: 'Drink more water'
+		}
+	],
 	setSearchQuery: (searchQuery: string) => set({ searchQuery }),
 	deleteTodo(id: number) {
-		set()
+		set(state => {
+			return { todos: state.todos.filter(todo => todo.id !== id) }
+		})
 	}
 })))
