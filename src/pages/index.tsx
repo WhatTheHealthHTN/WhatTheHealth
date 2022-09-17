3ca484bd2082b976173ac1fbc20ee5e277ef4476
Leon Si <leon@leonzalion.com>
2022-09-17 09:10:23 -0400

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,29 @@
+import { Button } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import { motion } from 'framer-motion'
+
+const HomeHeader = () => {
+	return (
+		<div className='flex flex-row py-2 px-4 gap-2 items-center'>
+			<div className='font-bold'>Health App</div>
+			<div className='flex-1'></div>
+			<Button>Login</Button>
+			<Button>Sign up</Button>
+		</div>
+	)
+}
 
 const Home: NextPage = () => {
 	return (
-		<div className="bg-black min-h-screen text-white">
-			<div className="text-center font-bold text-6xl">
+		<div className="min-h-screen">
+			<HomeHeader />
+			<motion.div className="text-center font-bold text-6xl text-gray-700 py-20 px-8 max-w-3xl mx-auto" initial={{ opacity: 0, top: 40 }} animate={{ opacity: 1, top: 0 }}>
 				Understand your{" "}
 				<span className="from-[#ff0000] to-[#ff8888] bg-clip-text bg-gradient-to-r text-transparent">
 					health
 				</span>
-				, fully
-			</div>
+				, fully.
+			</motion.div>
 			<div></div>
 		</div>
 	);
