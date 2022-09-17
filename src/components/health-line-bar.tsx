a5891b7b664515759ecfbfefcc76eea8c9f85fc9
Leon Si <leon@leonzalion.com>
2022-09-17 18:11:54 -0400

    colorized health diagnostics

--- src/components/health-line-bar.tsx
+++ src/components/health-line-bar.tsx
@@ -1,18 +1,21 @@
 import { motion } from 'framer-motion'
 
-export function HealthLineBar({ text, value, color }: { text: string, value: number, color: string }) {
+export function HealthLineBar({ text, value, fromColor, toColor }: { text: string, value: number, fromColor: string, toColor: string }) {
+	const linearGradientStyle = { background: `linear-gradient(90deg, ${fromColor}, ${toColor})` };
+
+	const reverseLinearGradientStyle = { background: `linear-gradient(90deg, ${toColor}, ${fromColor})` };
 	return (
 		<div className='flex flex-col'>
-			<div className='flex flex-row self-stretch px-2'>
-				<div className='font-bold'>{text}</div>
+			<div className='flex flex-row self-stretch'>
+				<div className='font-bold text-xl !bg-clip-text text-transparent' style={reverseLinearGradientStyle}>{text}</div>
 				<div className='flex-1'></div>
-				<div className='font-bold'>{value}</div>
+				<div className='font-bold text-xl !bg-clip-text text-transparent' style={linearGradientStyle}>{value}</div>
 			</div>
 
 			<div className='relative'>
-				<div className='h-2 opacity-20 self-stretch rounded-full' style={{ backgroundColor: color }}>
+				<div className='h-2 opacity-20 self-stretch rounded-full' style={{ backgroundColor: fromColor }}>
 				</div>
-				<motion.div initial={{ width: 0 }} animate={{ width: `${value}%` }} transition={{ duration: 1 }} className='absolute inset-0 z-1 rounded-md' style={{ backgroundColor: color }}></motion.div>
+				<motion.div initial={{ width: 0 }} animate={{ width: `${value}%` }} transition={{ duration: 1 }} style={{ background: `linear-gradient(90deg, ${fromColor}, ${toColor})` }} className={`absolute inset-0 z-1 rounded-md `}></motion.div>
 			</div>
 		</div>
 	)
--- src/components/health-pie-chart.tsx
+++ src/components/health-pie-chart.tsx
@@ -3,37 +3,40 @@ import { PieChart, Pie, Cell } from "recharts";
 
 export function HealthPieChart({ data }: { data: Array<{ value: number, color: string }> }) {
 	return (
-		<div className="relative flex flex-row justify-center items-center">
-			<PieChart width={150} height={150}>
-				<defs>
-					<linearGradient id={`myGradient0`}>
-						<stop
-							offset="0%"
-							stopColor='#4bb54360'
-						/>
-						<stop
-							offset="100%"
-							stopColor='#4bb543'
-						/>
-					</linearGradient>
-				</defs>
-				<circle cx='50%' cy='50%' r={42} strokeWidth={5} stroke='#aaaaaa30' fill='transparent'></circle>
-				<circle cx='50%' cy='50%' r={58} strokeWidth={5} stroke='#aaaaaa20' fill='transparent'></circle>
-				<circle cx='50%' cy='50%' r={50} strokeWidth={10} stroke='#4bb54340' fill='transparent'></circle>
-				<Pie dataKey='value' data={data} cx='50%' cy='50%' innerRadius={45} outerRadius={55} startAngle={-270} endAngle={360} animationBegin={0}>
-					{data.map((entry, index) => (
-						<Cell key={`cell-${index}`} fill={entry.color === 'transparent' ? 'transparent' : `url(#myGradient0)`} stroke='transparent' strokeLinecap='round' />
-					))}
-				</Pie>
-			</PieChart>
+		<div className='flex flex-col items-center mx-auto'>
+			<div className='font-bold text-2xl !bg-clip-text text-transparent from-[#ffd580] to-[#ff8c00] bg-gradient-to-r -mb-2'>Overall Score</div>
+			<div className="relative flex flex-row justify-center items-center">
+				<PieChart width={150} height={150}>
+					<defs>
+						<linearGradient id={`myGradient0`}>
+							<stop
+								offset="0%"
+								stopColor='#ffd580'
+							/>
+							<stop
+								offset="100%"
+								stopColor='#ff8c00'
+							/>
+						</linearGradient>
+					</defs>
+					<circle cx='50%' cy='50%' r={42} strokeWidth={5} stroke='#aaaaaa30' fill='transparent'></circle>
+					<circle cx='50%' cy='50%' r={58} strokeWidth={5} stroke='#aaaaaa20' fill='transparent'></circle>
+					<circle cx='50%' cy='50%' r={50} strokeWidth={10} stroke='#ffd58040' fill='transparent'></circle>
+					<Pie dataKey='value' data={data} cx='50%' cy='50%' innerRadius={45} outerRadius={55} startAngle={-270} endAngle={360} animationBegin={0}>
+						{data.map((entry, index) => (
+							<Cell key={`cell-${index}`} fill={entry.color === 'transparent' ? 'transparent' : `url(#myGradient0)`} stroke='transparent' strokeLinecap='round' />
+						))}
+					</Pie>
+				</PieChart>
 
-			<CountUp
-				className='absolute text-4xl from-[#4bb543] to-[#4bb54399] bg-clip-text text-transparent bg-gradient-to-r font-bold'
-				useEasing={true}
-				start={0}
-				end={92}
-				duration={1.5}
-			/>
+				<CountUp
+					className='absolute text-4xl from-[#ffd580] to-[#ff8c00] bg-clip-text text-transparent bg-gradient-to-r font-bold'
+					useEasing={true}
+					start={0}
+					end={92}
+					duration={1.5}
+				/>
+			</div>
 		</div>
 	)
 }
