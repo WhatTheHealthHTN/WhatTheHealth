8b22263cf8f3160666cadba03a1fc3c84841e810
Leon Si <leon@leonzalion.com>
2022-09-18 05:38:39 -0400

    login page animation

--- next-app/src/pages/login.tsx
+++ next-app/src/pages/login.tsx
@@ -1,11 +1,12 @@
 import { FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { FaArrowRight } from "react-icons/fa";
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 export default function LoginPage() {
 	return (
 		<div className='flex flex-col justify-center items-center h-screen w-screen bg-gradient-to-r from-[#E338F9aa] to-[#FEC93Aaa]'>
-			<div className='p-7 w-[400px] border rounded-lg bg-[rgb(251,250,255)] shadow-md'>
+			<motion.div className='p-7 w-[400px] relative border rounded-lg bg-[rgb(251,250,255)] shadow-md' initial={{ opacity: 0, top: -10 }} animate={{ opacity: 1, top: 0 }}>
 				<div className='from-[#E338F9] to-[#FEC93A] bg-clip-text bg-gradient-to-r text-transparent font-bold text-6xl mb-2 min-h-[70px] text-center'>Login</div>
 				<FormControl className='flex flex-col'>
 					<FormLabel>Email address</FormLabel>
@@ -16,7 +17,7 @@ export default function LoginPage() {
 						<button className='text-white rounded-md px-4 py-2 font-bold bg-gradient-to-r from-[#E338F9] to-[#FEC93A] hover:opacity-70 transition-all text-lg mx-auto mt-6 self-center flex flex-row items-center gap-2'>Enter What the Health <FaArrowRight /></button>
 					</Link>
 				</FormControl>
-			</div>
+			</motion.div>
 		</div>
 	)
 }
